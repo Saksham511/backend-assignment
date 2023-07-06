068b8fd81f00b810028e36f5dd95448587ab14ee
Saksham511 <sakshamshrestha511@gmail.com>
2023-07-06 07:32:29 +0545

    install and configure dotenv, use PORT in .env file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app= express();
+dotenv.config();
 
-app.listen(4002,console.log("Server is listening at port: 4002"));
+app.listen(process.env.PORT,console.log(`Server is listening at port: ${process.env.PORT}`));
 
 console.log("Hello from backend");
